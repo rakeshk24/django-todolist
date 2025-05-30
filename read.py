f6e73bef679b7aaa278b2b3fd80c02b1cad3c9ba
Rakesh Krishna <rakeshk.24@gmail.com>
2025-05-29 22:24:54 -0700

    Adding read.py

---
+++ read.py
@@ -0,0 +1,13 @@
+# names = []
+
+# with open("names.txt") as file:
+#     for line in file:
+#         names.append(line.rstrip())
+
+# for line in sorted(names):
+#     print(line)
+
+with open("names.csv") as file:
+    for line in file:
+        name,age = line.rstrip().split(",")
+        print(f"{name}'s age is {age}")
