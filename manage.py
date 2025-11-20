4024eaed10fc90f8e8ae55d2c4db06080652f9a9
rakeshk24 <rakeshk.24@gmail.com>
2025-11-20 13:37:42 -0800

    another print before execute

--- manage.py
+++ manage.py
@@ -14,6 +14,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    print("Executing command line arguments...")
     execute_from_command_line(sys.argv)
 
 
